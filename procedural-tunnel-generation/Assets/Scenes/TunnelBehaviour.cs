747c5109d08447267b4978163a012c1d320ce89b
Anu Sridhar <anu.sridhar@nearmap.com>
2019-01-31 14:16:24 +1100

    Added tunnel behaviour script

---
+++ procedural-tunnel-generation/Assets/Scenes/TunnelBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelBehaviour : MonoBehaviour
+{
+    public GameObject tunnelObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Testing object behaviour");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

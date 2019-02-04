b9ed6570d7a69246d3cd44a19a941ef8012076ee
Anu Sridhar <anu.sridhar@nearmap.com>
2019-02-04 21:45:14 +1100

    Added tunnel rendering

--- procedural-tunnel-generation/Assets/Scenes/TunnelBehaviour.cs
+++ procedural-tunnel-generation/Assets/Scenes/TunnelBehaviour.cs
@@ -4,16 +4,75 @@ using UnityEngine;
 
 public class TunnelBehaviour : MonoBehaviour
 {
-    public GameObject tunnelObject;
+    public Camera cameraObject;
+
+    Mesh mesh;
+
+    /*
+     * Number of segments to render along the length of the tunnel.
+     */   
+    public int numSegments;
+
+    /**
+     * Number of sectors to render around the tunnel.
+     */
+    public int numSectors;
+
+    public float length;
+
+    public float radius;
+
     // Start is called before the first frame update
     void Start()
     {
-        print("Testing object behaviour");
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        mesh = new Mesh();
+        if (meshFilter != null)
+        {
+            print("Mesh filter found");
+            meshFilter.mesh = mesh;
+        }
+
+        Vector3[] vertices = new Vector3[numSegments * numSectors];
+        int vertexIndex = 0;
+        float sectorAngle = (2 * Mathf.PI) / numSectors;
+        for (int r = 0; r < numSegments; r++)
+        {
+            float z = (((float)r / (float)numSegments) - 0.5f) * length;
+            for (int c = 0; c < numSectors; c++)
+            {
+                float theta = sectorAngle * c;
+                float x = radius * Mathf.Cos(theta);
+                float y = radius * Mathf.Sin(theta);
+                vertices[vertexIndex] = new Vector3(x, y, z);
+                vertexIndex++;
+            }
+        }
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[numSegments * numSectors * 6];
+        int triangleIndex = 0;
+        for (int r = 0; r < (numSegments - 1); r++)
+        {
+            for (int c = 0; c < (numSectors - 1); c++)
+            {
+                triangles[triangleIndex++] = r * numSectors + c;
+                triangles[triangleIndex++] = (r + 1) * numSectors + c;
+                triangles[triangleIndex++] = r * numSectors + c + 1;
+
+                triangles[triangleIndex++] = r * numSectors + c + 1;
+                triangles[triangleIndex++] = (r + 1) * numSectors + c;
+                triangles[triangleIndex++] = (r + 1) * numSectors + (c + 1);
+
+            }
+        }
+        print("Created " + triangleIndex + " triangles");
+        mesh.triangles = triangles;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
